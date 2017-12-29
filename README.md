a758bf84ad0c4f9b97f3d1399b052546f03d2b2d
Eric Shull <eric@exupero.org>
2017-12-28 21:55:07 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,10 @@
+# neovim
+
+A thin wrapper around [neovim-client](git@github.com:exupero/neovim.git) that builds API calls via data, which can then be used with `nvim_call_atomic`.
+
+## License
+
+Copyright © 2017 Eric Shull
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject org.exupero/neovim "0.1.0-SNAPSHOT"
+  :description "Clojure API for Neovim"
+  :url "https://github.com/exupero/neovim"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [neovim-client "0.1.1"]])
